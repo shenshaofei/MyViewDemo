05399152b5a0dcdf7071b6a987b6bdb064409672
shenshaofei <shaofei.shen@ecarx.com.cn>
2021-01-14 17:28:10 +0800

    继承自view 控件添加注释

--- MyViewDemo/app/src/main/java/com/example/demotestecarx/view/RectView.java
+++ MyViewDemo/app/src/main/java/com/example/demotestecarx/view/RectView.java
@@ -14,6 +14,8 @@ import androidx.annotation.Nullable;
  * 自定义view继承自view 重写onMeasure(),onDraw()
  * onMeasure() 获取自身的测量尺寸
  * onDraw() 刨除 父控件的padding限制，通过Paint 根据测量尺寸绘制矩形
+ *
+ * 继承自view的自定义控件需要重写 onMeasure ，onDraw ，不需要重写onLayout，因为view内部没有子控件只需要测量自身尺寸就可以
  * */
 public class RectView extends View {
 
@@ -34,6 +36,7 @@ public class RectView extends View {
     }
 
     private void init() {
+       // 初始化画笔，及填充颜色
         mPaint = new Paint();
         mPaint.setColor(Color.GRAY);
     }
@@ -46,6 +49,7 @@ public class RectView extends View {
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
+        // 根据测量mode 设置测量的view尺寸
         if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
             setMeasuredDimension(400,400);
         } else if (widthMode == MeasureSpec.AT_MOST) {
@@ -58,6 +62,7 @@ public class RectView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        // 获取父控件的padding尺寸
         int paddingLeft = getPaddingLeft();
         int paddingTop = getPaddingTop();
         int paddingRight = getPaddingRight();
@@ -68,6 +73,6 @@ public class RectView extends View {
         int height = getHeight() - paddingTop - paddingBottom;
 
         // 开始绘制矩形的区域
-        canvas.drawRect(paddingLeft, paddingTop, width + paddingLeft, height + paddingTop,mPaint);
+        canvas.drawRect(paddingLeft, paddingTop, width + paddingLeft, height + paddingTop, mPaint);
     }
 }
